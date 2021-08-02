9b3757dfc41ca6164d65545305efe4fbcfd54150
Claudio Cambra <claudio.cambra@gmail.com>
2021-08-02 13:17:42 +0200

    Adjusted colours

--- src/contents/ui/labelutils.js
+++ src/contents/ui/labelutils.js
@@ -156,6 +156,6 @@ function recurrenceEndToString(recurrenceData) {
 function isDarkColor(background) {
     var temp = Qt.darker(background, 1);
     var a = 1 - ( 0.299 * temp.r + 0.587 * temp.g + 0.114 * temp.b);
-    return temp.a > 0 && a >= 0.5;
+    return temp.a > 0 && a >= 0.4;
 }
 
