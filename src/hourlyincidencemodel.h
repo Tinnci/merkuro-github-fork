9a243b79a42a090a01ac57ba7afa04419a8a5cbc
Claudio Cambra <claudio.cambra@gmail.com>
2021-09-26 21:59:42 +0200

    Added initial (and likely questionable) HourlyIncidenceModel

---
+++ src/hourlyincidencemodel.h
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: 2021 Claudio Cambra <claudio.cambra@gmail.com>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+#pragma once
+
+#include <QAbstractItemModel>
+#include <QList>
+#include <QSet>
+#include <QSharedPointer>
+#include <QTimer>
+#include <QDateTime>
+#include "incidenceoccurrencemodel.h"
+
+namespace KCalendarCore {
+    class MemoryCalendar;
+    class Incidence;
+}
+
+/**
+ * Each toplevel index represents a day.
+ * The "incidences" roles provides a list of lists, where each list represents a visual line,
+ * containing a number of events to display.
+ */
+class HourlyIncidenceModel : public QAbstractItemModel
+{
+    Q_OBJECT
+    Q_PROPERTY(int periodLength READ periodLength WRITE setPeriodLength NOTIFY periodLengthChanged)
+    Q_PROPERTY(IncidenceOccurrenceModel* model WRITE setModel)
+
+public:
+    HourlyIncidenceModel(QObject *parent = nullptr);
+    ~HourlyIncidenceModel() = default;
+
+    QModelIndex index(int row, int column, const QModelIndex &parent = {}) const override;
+    QModelIndex parent(const QModelIndex &index) const override;
+
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+
+    QVariant data(const QModelIndex &index, int role) const override;
+
+    QHash<int, QByteArray> roleNames() const override;
+
+    void setModel(IncidenceOccurrenceModel *model);
+    int periodLength();
+    void setPeriodLength(int periodLength);
+
+Q_SIGNALS:
+    void periodLengthChanged();
+
+private:
+    QTimer mRefreshTimer;
+    QList<QModelIndex> sortedIncidencesFromSourceModel(const QDateTime &rowStart) const;
+    QVariantList layoutLines(const QDateTime &rowStart) const;
+    IncidenceOccurrenceModel *mSourceModel{nullptr};
+    int mPeriodLength{15};
+};
