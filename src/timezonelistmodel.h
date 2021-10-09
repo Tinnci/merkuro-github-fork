9986c9ab7b1a44e0598f9c0fa55d8a0f87ae8b67
Carl Schwan <carl@carlschwan.eu>
2021-10-09 13:13:01 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/timezonelistmodel.h
+++ src/timezonelistmodel.h
@@ -24,7 +24,7 @@ public:
     QHash<int, QByteArray> roleNames() const override;
     int rowCount(const QModelIndex &parent = {}) const override;
 
-    Q_INVOKABLE int getTimeZoneRow(const QByteArray timeZone);
+    Q_INVOKABLE int getTimeZoneRow(const QByteArray &timeZone);
 
 private:
     QVector<QByteArray> m_timeZones;
