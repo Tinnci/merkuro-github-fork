b6dd4cddb73c948b8225ca0a2d0b0072c99ed616
Claudio Cambra <claudio.cambra@gmail.com>
2022-01-15 12:46:55 +0100

    Simplify IncidenceOccurrenceModel and remove unused stuff from it

--- autotests/incidenceoccurrencemodeltest.cpp
+++ autotests/incidenceoccurrencemodeltest.cpp
@@ -1,1 +1,20 @@
+// SPDX-FileCopyrightText: 2021 Claudio Cambra <claudio.cambra@gmail.com>
+// SPDX-License-Identifier: LGPL-2.0-or-later
 
+#include "models/incidenceoccurrencemodel.h"
+
+#include <QTest>
+
+class IncidenceOccurrenceModelTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    IncidenceOccurrenceModelTest() = default;
+    ~IncidenceOccurrenceModelTest() = default;
+
+private:
+};
+
+QTEST_MAIN(IncidenceOccurrenceModelTest)
+#include "incidenceoccurrencemodeltest.moc"
