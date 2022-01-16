c4c94558d423c767a319f33571ee370b94c65fe2
Claudio Cambra <claudio.cambra@gmail.com>
2022-01-16 14:02:04 +0100

    Add testing for incidenceOccurrenceModel data func

--- autotests/incidenceoccurrencemodeltest.cpp
+++ autotests/incidenceoccurrencemodeltest.cpp
@@ -6,6 +6,8 @@
 #include <Akonadi/Calendar/IncidenceChanger>
 #include <KCalendarCore/Incidence>
 #include <KCheckableProxyModel>
+#include <KFormat>
+#include <QAbstractItemModelTester>
 #include <QSignalSpy>
 #include <QTest>
 #include <akonadi/qtest_akonadi.h>
@@ -36,8 +38,11 @@ signals:
 
 private:
     QScopedPointer<Akonadi::ETMCalendar> calendar;
+    IncidenceOccurrenceModel model;
+    QAbstractItemModelTester modelTester = QAbstractItemModelTester(&model);
     QTimer loadedCheckTimer;
     Akonadi::Collection collection;
+    QDateTime now = QDateTime::currentDateTime();
 
 private slots:
     void initTestCase()
@@ -46,7 +51,7 @@ private slots:
 
         calendar.reset(new Akonadi::ETMCalendar);
         QSignalSpy collectionsAdded(calendar.data(), &Akonadi::ETMCalendar::collectionsAdded);
-        QVERIFY(collectionsAdded.wait(5000));
+        QVERIFY(collectionsAdded.wait(10000));
         collection = collectionsAdded.takeFirst().at(0).value<Akonadi::Collection>();
         qDebug() << QStringLiteral("USING COLLECTION: ") << collection.name() << collection.id();
 
@@ -67,12 +72,9 @@ private slots:
 
     void testAddCalendar()
     {
-        QDateTime now = QDateTime::currentDateTime();
-
         QVERIFY(calendar->isLoaded());
         QVERIFY(calendar->items().count() > 0);
 
-        IncidenceOccurrenceModel model;
         QSignalSpy fetchFinished(&model, &QAbstractItemModel::modelReset);
 
         model.setStart(now.date());
@@ -81,7 +83,52 @@ private slots:
         model.updateQuery();
 
         fetchFinished.wait(10000);
+        // Our test calendar file has an event that recurs every day of the week.
+        // Since we are checking for 7 days, we should have an instance of this event
+        // 7 times.
         QCOMPARE(model.rowCount(), 7);
+    }
+
+    void testData()
+    {
+        // Check everything is still there from the previous test
+        QCOMPARE(model.rowCount(), 7);
+
+        // Test that the data function gives us the event info we have in our calendar file
+        const auto index = model.index(0, 0);
+        QVERIFY(index.data(IncidenceOccurrenceModel::Summary).toString() == QStringLiteral("Test event"));
+        QVERIFY(index.data(IncidenceOccurrenceModel::Description).toString() == QStringLiteral("Big testing description"));
+        QVERIFY(index.data(IncidenceOccurrenceModel::Location).toString() == QStringLiteral("Testing land"));
+
+        // It's an all day event
+        QDateTime eventStartTimeToday(now.date().startOfDay());
+        QCOMPARE(index.data(IncidenceOccurrenceModel::StartTime).toDateTime().toMSecsSinceEpoch(), eventStartTimeToday.toMSecsSinceEpoch());
+        QCOMPARE(index.data(IncidenceOccurrenceModel::EndTime).toDateTime().toMSecsSinceEpoch(), eventStartTimeToday.addDays(1).toMSecsSinceEpoch());
+
+        const auto duration = index.data(IncidenceOccurrenceModel::Duration).value<KCalendarCore::Duration>();
+        QCOMPARE(duration.asDays(), 1);
+        KFormat format;
+        QVERIFY(index.data(IncidenceOccurrenceModel::DurationString).toString() == format.formatSpelloutDuration(duration.asSeconds() * 1000));
+
+        QVERIFY(index.data(IncidenceOccurrenceModel::Recurs).toBool());
+        QVERIFY(index.data(IncidenceOccurrenceModel::HasReminders).toBool());
+        QCOMPARE(index.data(IncidenceOccurrenceModel::Priority).toInt(), 0);
+        QVERIFY(index.data(IncidenceOccurrenceModel::AllDay).toBool());
+
+        // CalendarManager generates the colors for different collections so let's skip that check, since it will give invalid
+
+        QCOMPARE(index.data(IncidenceOccurrenceModel::CollectionId).toLongLong(), 2);
+
+        QVERIFY(!index.data(IncidenceOccurrenceModel::TodoCompleted).toBool()); // An event should always return false for this
+        QVERIFY(!index.data(IncidenceOccurrenceModel::IsOverdue).toBool());
+        QVERIFY(!index.data(IncidenceOccurrenceModel::IsReadOnly).toBool());
+
+        QVERIFY(!index.data(IncidenceOccurrenceModel::IncidenceId).toString().isNull());
+        QCOMPARE(index.data(IncidenceOccurrenceModel::IncidenceType).toInt(), KCalendarCore::Incidence::TypeEvent);
+        QVERIFY(index.data(IncidenceOccurrenceModel::IncidenceTypeStr).toString() == QStringLiteral("Event"));
+
+        QVERIFY(index.data(IncidenceOccurrenceModel::IncidencePtr).canConvert<KCalendarCore::Incidence::Ptr>());
+        QVERIFY(index.data(IncidenceOccurrenceModel::IncidenceOccurrence).canConvert<IncidenceOccurrenceModel::Occurrence>());
     }
 };
 
