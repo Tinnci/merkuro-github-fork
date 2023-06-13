468671ce8f306df83e34380429cc0ea1658ca103
Carl Schwan <carl@carlschwan.eu>
2023-06-14 01:13:14 +0200

    Split Calendar and address book in two seperate app

--- src/calendar/utils.h
+++ src/calendar/utils.h
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2023 Carl Schwan <carl@carlschwan.eu>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
+#include <KCalendarCore/Duration>
+#include <KFormat>
 #include <QObject>
 
 class Utils : public QObject
@@ -12,4 +14,6 @@ public:
 
     /// Gives prettified time
     Q_INVOKABLE QString secondsToReminderLabel(const qint64 seconds) const;
+
+    Q_REQUIRED_RESULT static QString formatSpelloutDuration(const KCalendarCore::Duration &duration, const KFormat &format, bool allDay);
 };
