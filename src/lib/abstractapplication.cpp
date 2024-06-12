62cfffd02cf086dfeb7ece712c42c44b4002df38
Carl Schwan <carl@carlschwan.eu>
2024-06-12 02:33:20 +0200

    Fix toggling menubar

--- src/lib/abstractapplication.cpp
+++ src/lib/abstractapplication.cpp
@@ -11,8 +11,11 @@
 #include <QGuiApplication>
 #include <QMenu>
 
+using namespace Qt::StringLiterals;
+
 AbstractApplication::AbstractApplication(QObject *parent)
     : KirigamiAbstractApplication(parent)
+    , m_shared(KSharedConfig::openConfig())
 {
 }
 
@@ -32,6 +35,33 @@ void AbstractApplication::setupActions()
         openTagManagerAction->setText(i18n("Manage Tags…"));
         openTagManagerAction->setIcon(QIcon::fromTheme(QStringLiteral("action-rss_tag")));
     }
+
+    actionName = QLatin1StringView("toggle_menubar");
+    if (KAuthorized::authorizeAction(actionName)) {
+        auto action = mainCollection()->addAction(actionName, this, &AbstractApplication::toggleMenubar);
+        action->setText(i18n("Show Menubar"));
+        action->setIcon(QIcon::fromTheme(QStringLiteral("show-menu")));
+        action->setCheckable(true);
+        KConfigGroup config(m_shared, u"General"_s);
+        action->setChecked(config.readEntry(u"showMenubar"_s, true));
+        mainCollection()->setDefaultShortcut(action, Qt::CTRL | Qt::Key_M);
+    }
+}
+
+void AbstractApplication::toggleMenubar()
+{
+    KConfigGroup config(m_shared, u"General"_s);
+    auto state = !config.readEntry(u"showMenubar"_s, true);
+    config.writeEntry(u"showMenubar"_s, state);
+    m_shared->sync();
+
+    Q_EMIT menubarVisibleChanged();
+}
+
+bool AbstractApplication::menubarVisible() const
+{
+    KConfigGroup config(m_shared, u"General"_s);
+    return config.readEntry(u"showMenubar"_s, true);
 }
 
 #include "moc_abstractapplication.cpp"
--- src/lib/abstractapplication.h
+++ src/lib/abstractapplication.h
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 #pragma once
 
+#include <KSharedConfig>
 #include <KirigamiAbstractApplication>
 #include <QObject>
 #include <QSortFilterProxyModel>
@@ -10,13 +11,23 @@ class AbstractApplication : public KirigamiAbstractApplication
 {
     Q_OBJECT
 
+    /// This property holds whether the menubar is visible.
+    Q_PROPERTY(bool menubarVisible READ menubarVisible NOTIFY menubarVisibleChanged)
+
 public:
     explicit AbstractApplication(QObject *parent = nullptr);
+    void toggleMenubar();
+
+    bool menubarVisible() const;
 
 Q_SIGNALS:
     void openSettings();
     void openTagManager();
+    void menubarVisibleChanged();
 
 protected:
     void setupActions() override;
+
+private:
+    KSharedConfig::Ptr m_shared;
 };
